ca1b29008e78b4042d15dcffd91f63b3cf96968a
Oliver Chalk <oliverchalk245@gmail.com>
2021-08-05 14:25:40 +1000

    Eslint: Turn no-magic-numbers on

--- .eslintrc.js
+++ .eslintrc.js
@@ -182,7 +182,7 @@ module.exports = {
         "no-console": "error",                                                                              // Disallow calls to console.log
         "no-duplicate-imports": "error",                                                                    // Imports from the same file must be merged
         "no-irregular-whitespace": "error",                                                                 // Disallow weird whitespace characters
-        "no-magic-numbers": "off",  // TODO: Turn on                                                        // Disallow number literals
+        "no-magic-numbers": "error",                                                                        // Disallow number literals
         "no-multiple-empty-lines": "error",                                                                 // Allow at most one empty line between code
         "no-trailing-spaces": "error",                                                                      // Strip whitespace after line ends
         "object-curly-spacing": ["error", "always"], // TODO: Test { a:0 }                                  // Requires spaces like: { a: 0 }
