1fcf807bec22e6ab47b5641798bcc224036b9e5a
Oliver Chalk <oliverchalk245@gmail.com>
2021-12-17 22:47:06 -0500

    feat(gulp): add check-node script to validate node version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,4 @@
+const util          = require("util");
 const fs            = require("fs");
 const path          = require("path");
 const _             = require("lodash");
@@ -25,6 +26,23 @@ const SrcPath = (aPath = "") => path.join(SrcFolder, aPath);
 const DistDest = (aPath = "") => { return gulp.dest(DistPath(aPath)); }
 const Root = (aPath = "") => { return gulp.src(RootPath(aPath)); }
 const Src = (aPath = "") => { return gulp.src(SrcPath(aPath)); }
+
+// ---------------------------------------------------------------------------------------------------------------------
+gulp.task("check-node", (done) =>
+{
+    const lExpected = fs.readFileSync(`${RootFolder}/.nvmrc`).toString();
+    const lActual = process.version;
+
+    if (lActual.trim() !== lExpected.trim())
+    {
+        console.log("INVALID NODE VERSION");
+        console.log("EXPECTED:", lExpected);
+        console.log("RECEIVED:", lActual);
+        done(-1);
+    }
+
+    done(0);
+});
 
 // ---------------------------------------------------------------------------------------------------------------------
 gulp.task("clean", (done) =>
