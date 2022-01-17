2eda9224282052b420e5fc206d7a51ef48f5ce26
Oliver Chalk <oliverchalk245@gmail.com>
2022-01-17 23:22:42 +0100

    chore(deps): update to latest, point at oss gulp-tasks

--- Gulpfile.ts
+++ Gulpfile.ts
@@ -16,32 +16,32 @@ import {
     MDLintCheck,
     MDLintFix,
     Test,
-} from "@proximacapital/gulp-tasks";
+} from "@proxima-oss/gulp-tasks";
 import gulp, { TaskFunctionCallback } from "gulp";
 
+// ***** Base Tasks *****
+gulp.task("build", Build);
 gulp.task("check-node", CheckNode);
 gulp.task("clean", Clean);
 gulp.task("compile", Compile);
-gulp.task("copy", Copy);
-gulp.task("build", Build);
-gulp.task("eslint-check", ESLintCheck);
-gulp.task("eslint-fix", ESLintFix);
-gulp.task("mdlint-check", MDLintCheck);
-gulp.task("mdlint-fix", MDLintFix);
-gulp.task("lint-check", Lint);
-gulp.task("lint-fix", LintFix);
-gulp.task("test", Test);
-gulp.task("demo", Demo);
 gulp.task("coverage", Coverage);
+gulp.task("copy", Copy);
+gulp.task("demo", Demo);
+gulp.task("eslint", ESLintCheck);
+gulp.task("eslint-fix", ESLintFix);
+gulp.task("lint", Lint);
+gulp.task("lint-fix", LintFix);
+gulp.task("mdlint", MDLintCheck);
+gulp.task("mdlint-fix", MDLintFix);
+gulp.task("test", Test);
 
+// ***** Custom Tasks *****
 gulp.task("start", (done: TaskFunctionCallback): void =>
 {
     ExecTask("node Dist/Src/App.js", done);
 });
 
-// ----------
-// Aliases:
+// ***** Aliases *****
 gulp.task("tests-all", gulp.task("test"));
 gulp.task("build-test", BuildTest);
-gulp.task("build-check-test", BuildCheckTest);
 gulp.task("build-lint-test", BuildCheckTest);
